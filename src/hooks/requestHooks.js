d72b08197983f8629f14513aa0b7c83701c65cc5
Philip Van Raalte <phil.vanraalte@gmail.com>
2019-07-13 14:22:16 -0400

    Sorting (#2)
    * added sorting
    
    * used cancel token
    
    * added sort arrow
    
    * adjusted arrow size

--- src/hooks/requestHooks.js
+++ src/hooks/requestHooks.js
@@ -1,12 +1,12 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 
-export const useRequest = ({ query, variables = null }) => {
+export const useRequest = ({ query, variables = null }, deps = []) => {
   const [data, setData] = useState({});
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState();
-
   useEffect(() => {
+    const source = axios.CancelToken.source();
     axios
       .post(
         `${process.env.HOST_URL}/graphql`,
@@ -17,7 +17,8 @@ export const useRequest = ({ query, variables = null }) => {
         {
           headers: {
             "Content-Type": "application/json"
-          }
+          },
+          cancelToken: source.token
         }
       )
       .then(res => {
@@ -30,10 +31,14 @@ export const useRequest = ({ query, variables = null }) => {
         setIsLoading(false);
       })
       .catch(error => {
+        if (axios.isCancel(error)) return;
+
         setError(error);
         setIsLoading(false);
       });
-  }, []);
+
+    return () => source.cancel();
+  }, deps);
 
   return [data, isLoading, error];
 };
