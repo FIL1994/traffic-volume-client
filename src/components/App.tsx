769cb245f1756208a521438e11cb828bb5656b70
Philip Van Raalte <phil.vanraalte@gmail.com>
2019-09-23 22:07:31 -0400

    changed components to typescript

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import TrafficList from "./TrafficList";
 import "./app.less";
 import Traffic from "./Traffic";
