7275cb9ae24a805658742e9dc3553fb9d9243be4
Philip Van Raalte <phil.vanraalte@gmail.com>
2019-09-23 21:48:39 -0400

    setup typescript

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,11 @@
 module.exports = api => {
   api.cache(true);
   return {
-    presets: ["@babel/preset-env", "@babel/preset-react"],
+    presets: [
+      "@babel/preset-env",
+      "@babel/preset-react",
+      "@babel/preset-typescript"
+    ],
     plugins: [
       "@babel/plugin-transform-runtime",
       "@babel/plugin-syntax-dynamic-import",
